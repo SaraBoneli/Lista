7ad24e72c2f7130a52d1a848c8875e24c2135458
20212tiot0090 <saraboneligoncalvescordeiro@gmail.com>
2024-04-16 14:47:13 -0300

    Passo 7 completo

--- app/src/main/java/sara/dayse/lista/activity/NewItemActivity.java
+++ app/src/main/java/sara/dayse/lista/activity/NewItemActivity.java
@@ -1,14 +1,16 @@
-package sara.dayse.lista;
+package sara.dayse.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import sara.dayse.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_item);
     }
 }
