16c4eefadf6038db9eec9f36a8909302b7b3d5ef
20212tiot0090 <saraboneligoncalvescordeiro@gmail.com>
2024-04-23 16:03:12 -0300

    Passo 11 completo

--- app/src/main/java/sara/dayse/lista/activity/MainActivity.java
+++ app/src/main/java/sara/dayse/lista/activity/MainActivity.java
@@ -2,16 +2,22 @@ package sara.dayse.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import sara.dayse.lista.R;
+import sara.dayse.lista.model.MyItem;
 
 public class MainActivity extends AppCompatActivity {
     static int NEW_ITEM_REQUEST =1;
+    List<MyItem> itens = new ArrayList<>();//lista que está definida como atributo de MainActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_ITEM_REQUEST) { //se a condição de retorno foi cumprida
+            if (resultCode == Activity.RESULT_OK) {//se a condição de retorno foi cumprida
+                MyItem myItem = new MyItem();//cria uma instância de MyItem para guardar os dados do item
+                myItem.title = data.getStringExtra("title");//obtém os dados retornados por NewItemActivity
+                // e os guarda dentro de myItem
+                myItem.description = data.getStringExtra("description");//obtém os dados retornados por NewItemActivity e os
+                //guarda dentro de myItem
+                myItem.photo = data.getData();//obtém os dados retornados por NewItemActivity e os
+                //guarda dentro de myItem
+                itens.add(myItem);//adiciona o item a uma lista de itens
+            }
+        }
     }
 }
---
+++ app/src/main/java/sara/dayse/lista/model/MyItem.java
@@ -0,0 +1,9 @@
+package sara.dayse.lista.model;
+
+import android.net.Uri;
+
+public class MyItem {
+    public Uri photo;//atributo do tipo Uri, guarda o endereço da imagem do item.
+    public String title;//atributo do tipo String, guarda o título do item.
+    public String description;//atributo do tipo String, guarda a descrição do item.
+}
